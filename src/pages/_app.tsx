290a193329091977737b76cef65d45ca276daf30
nurfaidzy <nurfaidzy@gmail.com>
2025-09-28 19:52:10 +0700

    feat: add background color and text color to main app container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { NotifHeader, Header, Footer } from "@/layouts";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className="bg-white text-black">
       <NotifHeader />
       <Header />
       <Component {...pageProps} />
